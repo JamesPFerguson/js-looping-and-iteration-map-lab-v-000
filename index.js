97edb9d9e262995ff13610de51f6d429713fd79a
James Ferguson <ncjamesferguson@gmail.com>
2018-05-09 20:05:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function lowerCaseDrivers (drivers) {
   return drivers.map(function (driver)
    { return driver.toLowerCase()});
 }
+
+function nameToAttributes(drivers) {
+  return drivers.map(function (driver) {
+    nameArr = driver.split(" ")
+    let fName = nameArr[0]
+    let lName = nameArr[1]
+    return {
+      firstName: fName;
+      lastName: lName;
+    }
+  })
+}
